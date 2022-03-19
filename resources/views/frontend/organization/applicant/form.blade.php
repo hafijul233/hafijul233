b50d0fd11e0670f331154a5e0653c1fa984a0dc0
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-03-20 00:20:48 +0600

    form style updated

--- resources/views/frontend/organization/applicant/form.blade.php
+++ resources/views/frontend/organization/applicant/form.blade.php
@@ -381,6 +381,17 @@
                         {
                             required: true
                         }
+                },
+                errorElement: 'span',
+                errorPlacement: function (error, element) {
+                    error.addClass('invalid-feedback');
+                    element.closest('.form-group').append(error);
+                },
+                highlight: function (element, errorClass, validClass) {
+                    $(element).removeClass('is-valid').addClass('is-invalid');
+                },
+                unhighlight: function (element, errorClass, validClass) {
+                    $(element).removeClass('is-invalid').addClass('is-valid');
                 }
             });
         });
