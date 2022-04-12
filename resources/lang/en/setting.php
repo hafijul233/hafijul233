c98e9a61e9898ad93aeee876e5823be52168dbc8
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-04-12 19:44:57 +0600

    gender translation added

---
+++ resources/lang/en/setting.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Setting Language
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'Male' => 'Male',
+    'Female' => 'Female'
+];
