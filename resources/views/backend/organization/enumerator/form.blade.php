c5dbf1e7aa634107fcb559570cc642f6731edd27
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-04-17 21:52:37 +0600

    edit form updated

--- resources/views/backend/organization/enumerator/form.blade.php
+++ resources/views/backend/organization/enumerator/form.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         {!! \Form::hText('name', __('enumerator.Name'), old('name', $enumerator->name ?? null), true, 3) !!}
         {!! \Form::hText('name_bd', __('enumerator.Name(Bangla)'), old('name_bd', $enumerator->name_bd ?? null), true, 3) !!}
-        {!!  \Form::hRadio('gender_id', __('enumerator.Gender'), $genders, 1, true, 3) !!}
+        {!! \Form::hRadio('gender_id', __('enumerator.Gender'), $genders, old('gender_id', $enumerator->gender_id ?? 1), true, 3) !!}
         {!! \Form::hDate('dob', __('enumerator.Date of Birth'), old('dob', $enumerator->dob ?? null), true, 3) !!}
         {!! \Form::hText('father', __('enumerator.Father Name'), old('father', $enumerator->father ?? null), true, 3) !!}
         {!! \Form::hText('mother', __('enumerator.Mother Name'), old('mother', $enumerator->mother ?? null), true, 3) !!}
@@ -10,13 +10,14 @@
         {!! \Form::hTextarea('present_address', __('enumerator.Present Address'), old('present_address', $enumerator->present_address ?? null), true, 3) !!}
         {!! \Form::hTextarea('permanent_address', __('enumerator.Permanent Address'), old('permanent_address', $enumerator->permanent_address ?? null), true, 3) !!}
         {!! \Form::hSelect('exam_level', __('enumerator.Highest Educational Qualification'),$exam_dropdown,
-        old('exam_level', $enumerator->exam_level ?? 2), true, 3, ['placeholder' => __('enumerator.Please select highest educational qualification')]) !!}
+                            old('exam_level', $enumerator->exam_level ?? 2), true, 3,
+                            ['placeholder' => __('enumerator.Please select highest educational qualification')]) !!}
         {!! \Form::hNumber('mobile_1', __('enumerator.Mobile 1'), old('mobile_1', $enumerator->mobile_1 ?? null), true, 3) !!}
         {!! \Form::hNumber('mobile_2', __('enumerator.Mobile 2'), old('mobile_2', $enumerator->mobile_2 ?? null), true, 3) !!}
         {!! \Form::hText('email', __('enumerator.Email'), old('email', $enumerator->email ?? null), true, 3) !!}
         {!! \Form::hNumber('whatsapp', __('enumerator.Whatsapp Number'), old('whatsapp', $enumerator->whatsapp ?? null), true, 3) !!}
         {!! \Form::hUrl('facebook', __('enumerator.Facebook ID'), old('facebook', $enumerator->facebook ?? null), true, 3) !!}
-        {!! \Form::hCheckbox('survey_id', __('enumerator.Survey'), $surveys, old('survey_id', $enumerator->survey_id ?? []),
+        {!! \Form::hCheckbox('survey_id', __('enumerator.Survey'), $surveys, old('survey_id', $enumerator->surveys->pluck('id')->toArray() ?? []),
     true, 3, ['placeholder' => __("enumerator.Select a Survey Option")]) !!}
         <div class="row mt-3">
             <div class="col-12 justify-content-end d-flex">
