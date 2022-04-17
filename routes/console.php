1d954ae4a7a18c42176076022884ca233d165a05
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-04-17 14:43:47 +0600

    optimize clear added

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('cache-clear', function () {
+    $this->comment(Artisan::call('optimize:clear'));
+})->purpose('Display an inspiring quote');
