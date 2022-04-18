eedc03d78f62a937ddbce9c3f47f2f429d628083
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-04-18 17:31:20 +0600

    show page updated

--- resources/views/backend/organization/survey/show.blade.php
+++ resources/views/backend/organization/survey/show.blade.php
@@ -26,8 +26,8 @@
 
 @section('actions')
     {!! \Html::backButton('backend.organization.surveys.index') !!}
-    {!! \Html::modelDropdown('backend.organization.surveys', $survey->id, ['color' => 'success',
-        'actions' => array_merge(['edit'], ($survey->deleted_at == null) ? ['delete'] : ['restore'])]) !!}
+    {{--    {!! \Html::modelDropdown('backend.organization.surveys', $survey->id, ['color' => 'success',
+            'actions' => array_merge(['edit'], ($survey->deleted_at == null) ? ['delete'] : ['restore'])]) !!}--}}
 @endsection
 
 @section('content')
@@ -35,52 +35,28 @@
         <div class="row">
             <div class="col-12">
                 <div class="card card-default">
-                    <div class="card-header p-3">
-                        <ul class="nav nav-pills nav-justified" id="pills-tab" role="tablist">
-                            <li class="nav-item">
-                                <a class="nav-link active" id="pills-home-tab"
-                                   data-toggle="pill" href="#pills-home" role="tab"
-                                   aria-controls="pills-home" aria-selected="true">
-                                    <strong>Details</strong>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" id="pills-timeline-tab"
-                                   data-toggle="pill" href="#pills-timeline"
-                                   role="tab" aria-controls="pills-timeline"
-                                   aria-selected="false"><strong>Timeline</strong></a>
-                            </li>
-                        </ul>
-                    </div>
                     <div class="card-body min-vh-100">
-                        <div class="tab-content" id="pills-tabContent">
-                            <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
-                                 aria-labelledby="pills-home-tab">
-                                <div class="row mb-3">
-                                    <div class="col-md-6">
-                                        <label class="d-block">Name</label>
-                                        <p class="font-weight-bold">{{ $survey->name ?? null }}</p>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label class="d-block">Enabled</label>
-                                        <p class="font-weight-bold">{{ \App\Supports\Constant::ENABLED_OPTIONS[$survey->enabled] }}</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-12">
-                                        <label class="d-block">Remarks</label>
-                                        <p class="font-weight-bold">{{ $survey->remarks ?? null }}</p>
-                                    </div>
-                                </div>
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label class="d-block">Name</label>
+                                <p class="font-weight-bold">{{ $survey->name ?? null }}</p>
                             </div>
-                            <div class="tab-pane fade" id="pills-timeline" role="tabpanel" aria-labelledby="pills-timeline-tab">
-                                @include('layouts.partials.timeline', $timeline)
+                            <div class="col-md-6">
+                                <label class="d-block">Enabled</label>
+                                <p class="font-weight-bold">{{ \App\Supports\Constant::ENABLED_OPTIONS[$survey->enabled] }}</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <label class="d-block">Remarks</label>
+                                <p class="font-weight-bold">{{ $survey->remarks ?? null }}</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
     {!! \App\Supports\CHTML::confirmModal('Enumerator', ['delete', 'restore']) !!}
 @endsection
