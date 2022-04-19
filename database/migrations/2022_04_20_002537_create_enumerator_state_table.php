3b9a681514ac78072de062ee129a626ab0e5300b
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-04-20 00:27:56 +0600

    state migration created

---
+++ database/migrations/2022_04_20_002537_create_enumerator_state_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+class CreateEnumeratorStateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //Temporary Disable Foreign Key Constraints
+        Schema::disableForeignKeyConstraints();
+
+        //Table Structure
+        Schema::create('enumerator_state', function (Blueprint $table) {
+            $table->foreignId('enumerator_id')->index()->nullable();
+            $table->foreignId('state_id')->index()->nullable();
+            $table->string('type')->nullable();
+        });
+
+        //Temporary Disable Foreign Key Constraints
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //Temporary Disable Foreign Key Constraints
+        Schema::disableForeignKeyConstraints();
+
+        //Remove Table Structure
+        Schema::dropIfExists('enumerator_state');
+
+        //Temporary Disable Foreign Key Constraints
+        Schema::enableForeignKeyConstraints();
+    }
+}
