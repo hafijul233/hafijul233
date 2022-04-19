6cc3944c0483aea30d890983233d9e579a271169
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-04-19 11:19:44 +0600

    sider title change

--- resources/views/layouts/partials/menu-sidebar.blade.php
+++ resources/views/layouts/partials/menu-sidebar.blade.php
@@ -5,7 +5,7 @@
              class="brand-image img-circle elevation-3"
              style="opacity: .8">
         <span class="brand-text font-weight-light">
-            <strong>{{ config('app.name') }}</strong>
+            <strong>{{ config('backend.sidebar') }}</strong>
         </span>
     </a>
 
