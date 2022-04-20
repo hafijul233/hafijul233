ffdf88d8bf938a1f20d18dd0897f6d2cb99a8d5e
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-04-20 23:36:06 +0600

    type issue fixed

--- resources/views/backend/setting/catalog/show.blade.php
+++ resources/views/backend/setting/catalog/show.blade.php
@@ -35,54 +35,29 @@
         <div class="row">
             <div class="col-12">
                 <div class="card card-default">
-                    <div class="card-header p-3">
-                        <ul class="nav nav-pills nav-justified" id="pills-tab" role="tablist">
-                            <li class="nav-item">
-                                <a class="nav-link active" id="pills-home-tab"
-                                   data-toggle="pill" href="#pills-home" role="tab"
-                                   aria-controls="pills-home" aria-selected="true">
-                                    <strong>Details</strong>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" id="pills-timeline-tab"
-                                   data-toggle="pill" href="#pills-timeline"
-                                   role="tab" aria-controls="pills-timeline"
-                                   aria-selected="false"><strong>Timeline</strong></a>
-                            </li>
-                        </ul>
-                    </div>
                     <div class="card-body min-vh-100">
-                        <div class="tab-content" id="pills-tabContent">
-                            <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
-                                 aria-labelledby="pills-home-tab">
-                                <div class="row mb-3">
-                                    <div class="col-md-6">
-                                        <label class="d-block">Name</label>
-                                        <p class="font-weight-bold">{{ $catalog->name ?? null }}</p>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label class="d-block">Model</label>
-                                        <p class="font-weight-bold">{{ class_basename($catalog->model_type) }}</p>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label class="d-block">Location</label>
-                                        <p class="font-weight-bold">{{ $catalog->model_type }}</p>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label class="d-block">Enabled</label>
-                                        <p class="font-weight-bold">{{ \App\Supports\Constant::ENABLED_OPTIONS[$catalog->enabled] }}</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-12">
-                                        <label class="d-block">Remarks</label>
-                                        <p class="font-weight-bold">{{ $catalog->remarks ?? null }}</p>
-                                    </div>
-                                </div>
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label class="d-block">Name</label>
+                                <p class="font-weight-bold">{{ $catalog->name ?? null }}</p>
                             </div>
-                            <div class="tab-pane fade" id="pills-timeline" role="tabpanel" aria-labelledby="pills-timeline-tab">
-                                @include('layouts.partials.timeline', $timeline)
+                            <div class="col-md-6">
+                                <label class="d-block">Type</label>
+                                <p class="font-weight-bold">{{ \App\Supports\Constant::CATALOG_LABEL[$catalog->type] ?? null }}</p>
+                            </div>
+                            <div class="col-md-6">
+                                <label class="d-block">Location</label>
+                                <p class="font-weight-bold">{{ $catalog->model_type }}</p>
+                            </div>
+                            <div class="col-md-6">
+                                <label class="d-block">Enabled</label>
+                                <p class="font-weight-bold">{{ \App\Supports\Constant::ENABLED_OPTIONS[$catalog->enabled] }}</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <label class="d-block">Remarks</label>
+                                <p class="font-weight-bold">{{ $catalog->remarks ?? null }}</p>
                             </div>
                         </div>
                     </div>
