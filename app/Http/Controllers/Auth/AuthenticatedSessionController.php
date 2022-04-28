75cb04bfd557740b31725c01c01ede8892989e34
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-04-28 18:10:40 +0600

    default lang set to bd

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -51,7 +51,7 @@ class AuthenticatedSessionController extends Controller
 
         \Log::info("Login Info Tapping", $confirm);
         if ($confirm['status'] === true) {
-            Session::put('locale', 'en');
+            Session::put('locale', 'bd');
             notify($confirm['message'], $confirm['level'], $confirm['title']);
             return redirect()->route($confirm['landing_page']);
         }
