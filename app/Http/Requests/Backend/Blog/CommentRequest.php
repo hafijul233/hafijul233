4c936fc74b0f9b92350fac63a0bc9c0c18b56338
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-05-09 00:39:20 +0600

    blog request created

---
+++ app/Http/Requests/Backend/Blog/CommentRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\Backend\Blog;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * @class ProjectRequest
+ * @package App\Http\Requests\Backend\Portfolio
+ */
+class CommentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "start_date" => ["required", "date"],
+            "end_date" => ["nullable", "date"],
+            "name" => ["required", "string", "min:2", "max:255"],
+            "owner" => ["required", "string", "min:2", "max:255"],
+            "url" => ["nullable", "url", "min:2"],
+            "description" => ["nullable", "string"],
+            "associate" => ["nullable", "string"],
+            "image" => ["nullable", "image"]
+        ];
+    }
+}
