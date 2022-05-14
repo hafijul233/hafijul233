c528b178a56427f22a6d9a30d60b772de5e86f3a
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-05-15 01:40:57 +0600

    toggle carat disabled

--- resources/views/backend/htmls/model-dropdown.blade.php
+++ resources/views/backend/htmls/model-dropdown.blade.php
@@ -1,7 +1,7 @@
 @if(!empty($options['actions']))
     @canany([$resourceRouteName . '.edit', $resourceRouteName . '.destroy', $resourceRouteName . '.restore'])
         <div class="dropdown d-inline-block  m-1 m-md-0">
-            <button class="btn btn-{{ $options['color'] ?? 'warning' }} dropdown-toggle"
+            <button class="{{-- dropdown-toggle --}}btn btn-{{ $options['color'] ?? 'warning' }}"
                     type="button" id="dropdownMenuButton"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fa fa-sliders-h"></i>
