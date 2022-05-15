8d55b3ab1ef3f7c17857ae60be7b99f351e52eaf
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-05-15 13:46:54 +0600

    summernote editor style patched

--- resources/views/backend/layouts/includes/html-editor.blade.php
+++ resources/views/backend/layouts/includes/html-editor.blade.php
@@ -1,7 +1,25 @@
-@push('page-style')
+@push('plugin-style')
     <link rel="stylesheet" href="{{ asset('plugins/codemirror/codemirror.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ asset('plugins/codemirror/theme/monokai.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ asset('plugins/summernote/summernote-bs4.min.css') }}" type="text/css">
+@endpush
+
+@push('page-style')
+    <style>
+        .note-toolbar {
+            background-color: transparent !important;
+            border-bottom: 0px solid !important;
+        }
+
+        .note-statusbar {
+            background-color: transparent !important;
+            border-top: 0px solid !important;
+        }
+
+        .note-editor {
+            box-shadow: inset 0 0 0 transparent !important;
+        }
+    </style>
 @endpush
 
 @push('page-script')
